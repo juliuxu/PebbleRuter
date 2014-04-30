dc8a5ee39ab4fb17806e149f1313e84d00f7d14d
Julian Jark <juliuxu@gmail.com>
2014-04-30 15:29:34 +0200

    only one command at the time

--- pebble_ruter/src/js/main.js
+++ pebble_ruter/src/js/main.js
@@ -8,30 +8,42 @@ Pebble.addEventListener("ready",
 /**
  * Boolean to make sure we are only processing one command at the time
  */
-var sendingInProgress = false;
+
+var proccessingCommand = false;
 
 Pebble.addEventListener("appmessage",
   function(e) {
     console.log("Received message: " + JSON.stringify(e.payload, null, 4));
 
-    if (sendingInProgress) {
-      console.log("sendingInProgress is true, cannot process two commands at the same time");
+    if (proccessingCommand) {
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+      console.log("proccessingCommand is true! cannot do two things at the same time");
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       return;
     }
     else {
-      sendingInProgress = true;
+      proccessingCommand = true;
     }
+
+    var successCb = function(e) {
+      console.log("Function returned successfully!");
+      proccessingCommand = false;
+    }
+    var failureCb = function(e) {
+      console.log("Function returned failed!");
+      proccessingCommand = false;
+    }
+
 
     if(e.payload.hasOwnProperty("GET_STOPS")) {
       console.log("GET_STOPS");
-      putStops(e.payload["GET_STOPS"]);
+      putStops(e.payload["GET_STOPS"], successCb, failureCb);
     }
     if(e.payload.hasOwnProperty("GET_DEPARTURES")) {
       console.log("GET_DEPARTURES");
-      putDepartures(e.payload["GET_DEPARTURES_STOPID"], e.payload["GET_DEPARTURES"]);
+      putDepartures(e.payload["GET_DEPARTURES_STOPID"], e.payload["GET_DEPARTURES"], successCb, failureCb);
     }
 
-    sendingInProgress = false;
 
   }
 );
