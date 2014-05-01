243769b36a4b73c0100c0e54b7b1df72768327de
Julian Jark <juliuxu@gmail.com>
2014-05-01 14:52:57 +0200

    simple status update

--- pebble_ruter/src/js/stops.js
+++ pebble_ruter/src/js/stops.js
@@ -8,8 +8,8 @@ function putStops(ttype, successCb, failureCb) {
 	Ruter.GetUTM32Location(function(err, X, Y) {
 		if (err === null) {
 
-			// Notify that we got location
-			// Pebble.sendAppMessage({});
+			// Notify that we got the current location
+			Pebble.sendAppMessage({"PUT_STOPS_LOCATION_SUCCESS": 1});
 
 			// Get the closest stops
 			Ruter.GetClosestStopsByTransportType(X, Y, travel_type, function(err2, data) {
@@ -27,22 +27,30 @@ function putStops(ttype, successCb, failureCb) {
 					stopsdata.unshift(data.length);
 
 					if (data.length == 0) {
-						// Send empty message?
-					}
+						// Notify that we got no stops
+						Pebble.sendAppMessage({"PUT_STOPS_EMPTY": 1});
+						successCb(null);
 
-					// 3~3242424~Bislett~432424~Dalsberg~2334324~Majorstuen
-					Pebble.sendAppMessage({"PUT_STOPS": stopsdata.join("~")},
-						function(e){
-							successCb(e);
-						},
-						function(e){
-							failureCb(e);
-						}
-					);
+					}
+					else {
+						// 3~3242424~Bislett~432424~Dalsberg~2334324~Majorstuen
+						Pebble.sendAppMessage({"PUT_STOPS": stopsdata.join("~")},
+							function(e){
+								successCb(e);
+							},
+							function(e){
+								failureCb(e);
+							}
+						);
+					}
 
 				}
 				else {
 					console.log("An error occured getting stops");
+
+					// Notify that an error occurred
+					Pebble.sendAppMessage({"PUT_STOPS_ERROR": 1});
+
 					console.log(err2);
 					failureCb(err2)
 				}
@@ -52,6 +60,10 @@ function putStops(ttype, successCb, failureCb) {
 		}
 		else {
 			console.log("An error occured getting location");
+
+			// Notify that we didn't get the current location
+			Pebble.sendAppMessage({"PUT_STOPS_LOCATION_ERROR": 1});
+
 			failureCb(err);
 		}
 
